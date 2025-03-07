4479c08a927ae2809f874619208c39455485277a
Beverly Yip <beverly.yip.8000@gmail.com>
2025-03-07 13:28:27 -0500

    sound

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 document.getElementById("feedButton").addEventListener("click", function () {
   const catImageContainer = document.getElementById("catEat-container");
+  const crunchSound = new Audio("img/crunchy.mp3");
 
   catImageContainer.classList.remove("hidden");
+  crunchSound.play();
 
   setTimeout(function () {
     catImageContainer.classList.add("hidden");
-  }, 5000);
+  }, 7000);
 });
